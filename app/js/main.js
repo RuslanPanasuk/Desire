f0b28abce9a206e5ffad0143caea365acffe491b
Ruslan Panasiuk <panasuk.eetp@gmail.com>
2021-06-20 23:23:40 +0300

    contact page finished

--- app/js/main.js
+++ app/js/main.js
@@ -5,11 +5,19 @@ $(function () {
     $('.rightside-menu__close').on('click', function(){
         $('.rightside-menu').addClass('rightside-menu--close');
     });
+
     $('.top__slider').slick({
         dots: true,
         arrows: false,
         fade: true,
         autoplay: true,
+    });
+
+    $('.contact-slider').slick({
+        slidesToShow: 10,
+        slidesToScroll: 10,
+        dots: true,
+        arrows: false,        
     });
 
     var mixer = mixitup('.gallery__inner', {
